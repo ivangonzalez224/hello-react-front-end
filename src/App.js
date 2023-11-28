f4f169b8fcf7c324445015784188adfa5faef112
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-11-28 17:01:11 -0500

    Create the store and the greetings slice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Greetings from './Greetings';
+import Greetings from './components/Greetings';
 
 const App = () => (
   <section>
